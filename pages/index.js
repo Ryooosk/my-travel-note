a9b7eec689f4f90d1363e9c9269c2d0842a24295
sukesan3 <basikobasiko5@gmail.com>
2022-02-21 17:40:31 +0900

    second

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,45 @@
 import Head from 'next/head'
+import * as React from 'react'
+
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Lorem,
+} from '@chakra-ui/react'
+
+const BasicUsage = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Lorem count={2} />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
 
 export default function Home() {
   return (
@@ -9,6 +50,7 @@ export default function Home() {
       </Head>
       
       <main>
+        <BasicUsage></BasicUsage>
         <header>
           <div class="title">
             <h1>僕の旅行記</h1>
